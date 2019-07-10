e30fd03a48170bf74e621034018c817da5d0d881
Ricardo Espinoza <ricardoespinoza@Ricardos-MacBook-Pro.local>
2019-07-10 12:42:48 -0500

    mapa funcional

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  map: GoogleMap;
   constructor(public navCtrl: NavController) {
-
+    this.loadMap();
   }
+  loadMap() {
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyC_8QhTAOkJO5MpOIDbqlEvk4wFjYd4myQ',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyC_8QhTAOkJO5MpOIDbqlEvk4wFjYd4myQ'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+    }
 
 }
